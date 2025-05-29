6fc89b77230e0455d247c3d025a7b31c91457e6d
Lorezz <point.point@gmail.com>
2025-05-29 11:03:18 +0200

    feat: style and collections homepages :zap:

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,27 +1,28 @@
 import { defineCollection, z } from "astro:content";
-import { glob, file } from "astro/loaders";
+import { file } from "astro/loaders";
+// import { glob, file } from "astro/loaders";
 
-const news = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/contents/news" }),
-});
-const works = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/contents/works" }),
-});
+// const news = defineCollection({
+//   loader: glob({ pattern: "**/*.md", base: "./src/contents/news" }),
+// });
+// const works = defineCollection({
+//   loader: glob({ pattern: "**/*.md", base: "./src/contents/works" }),
+// });
 
-const events = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/contents/events" }),
-});
+// const events = defineCollection({
+//   loader: glob({ pattern: "**/*.md", base: "./src/contents/events" }),
+// });
 
-const pages = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/contents/pages" }),
-});
+// const pages = defineCollection({
+//   loader: glob({ pattern: "**/*.md", base: "./src/contents/pages" }),
+// });
 
-const config = defineCollection({
-  loader: file("./src/contents/data/config.json"),
-});
+// const config = defineCollection({
+//   loader: file("./src/contents/data/config.json"),
+// });
 
 const homepages = defineCollection({
   loader: file("./src/contents/data/homepages.json"),
 });
 
-export const collections = { pages, news, works, events, config, homepages };
+export const collections = { homepages };
